9859e5c4bd612fd84201a04866dac54ca40fdac4
Alexandr <leonov22.a.v@gmail.com>
2019-08-23 17:53:17 +0700

    Создание базы данных

---
+++ HotelReservation/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+	[ClientId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FIO] VARCHAR(50) NOT NULL, 
+    [PassportID] INT NOT NULL, 
+    [BirthdayDate] DATE NOT NULL, 
+    [Sex] BIT NOT NULL
+)
